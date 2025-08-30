6bbe87fe8fbf3d49d162725bfe39a253cac6df0c
owenwesley <59519554+owenwesley@users.noreply.github.com>
2025-08-29 20:49:58 -0700

    Meds goback #317

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1700,10 +1700,10 @@ export default class NavBar extends Component {
         await editReading(user, readings, editIdx);
       }
       {
-        chkMedsB ||
-          chkMedsL ||
-          chkMedsD ||
-          chkMedsBed
+        readings[editIdx].chkMedsB ||
+          readings[editIdx].chkMedsL ||
+          readings[editIdx].chkMedsD ||
+          readings[editIdx].chkMedsBed
           ? ({ totalAm, totalNoon, totalEvening, totalBed } = await deductMeds(
             readings,
             editIdx,
