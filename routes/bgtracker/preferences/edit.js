5560a998824c7c57cbfbd906df045072aa4be64d
Wesley Owen <owenwesley1976@gmail.com>
2026-03-17 10:50:11 -0700

    rebuild #320.1 03-17-26

--- routes/bgtracker/preferences/edit.js
+++ routes/bgtracker/preferences/edit.js
@@ -5,38 +5,44 @@ const connection = require('../../../db/Connection');
 router.post('/:id', (req, res) => {
   const {
     id = req.params.id,
-    user_id,
-    timesPD,
-    chkNutrition,
-    chkWeight,
-    height,
-    chkMeds,
-    chkMedsB,
-    chkMedsL,
-    chkMedsD,
-    chkMedsBed,
-    chkInsulin,
-    typInsulin,
-    chkBP,
-    chkSlidingScale,
-    slidingScale1,
-    slidingScale2a,
-    slidingScale2b,
-    slidingScale3a,
-    slidingScale3b,
-    slidingScale4a,
-    slidingScale4b,
-    slidingScale5,
-    carbRatio,
-  } = req.body;
-
-  const sql = `UPDATE preferences SET id=?, user_id=?, timesPD=?,
-   chkNutrition=?, chkWeight=?, height=?, chkMeds=?, chkMedsB=?,
-   chkMedsL=?, chkMedsD=?, chkMedsBed=?, chkInsulin=?, typInsulin=?,
-   chkBP=?, chkSlidingScale=?, slidingScale1=?, slidingScale2a=?,
-   slidingScale2b=?, slidingScale3a=?, slidingScale3b=?,
-   slidingScale4a=?, slidingScale4b=?, slidingScale5=?, carbRatio=?
-    WHERE id=? AND user_id=?;`;
+    user_id = req.body.user_id,
+    timesPD = req.body.timesPD,
+    chkNutrition = req.body.chkNutrition,
+    chkWeight = req.body.chkWeight,
+    height = req.body.height,
+    chkMeds = req.body.chkMeds,
+    chkMedsB = req.body.chkMedsB,
+    chkMedsL = req.body.chkMedsL,
+    chkMedsD = req.body.chkMedsD,
+    chkMedsBed = req.body.chkMedsBed,
+    chkInsulin = req.body.chkInsulin,
+    typInsulin = req.body.typInsulin,
+    chkBP = req.body.chkBP,
+    chkSlidingScale = req.body.chkSlidingScale,
+    slidingScale1 = req.body.slidingScale1,
+    slidingScale2a = req.body.slidingScale2a,
+    slidingScale2b = req.body.slidingScale2b,
+    slidingScale3a = req.body.slidingScale3a,
+    slidingScale3b = req.body.slidingScale3b,
+    slidingScale4a = req.body.slidingScale4a,
+    slidingScale4b = req.body.slidingScale4b,
+    slidingScale5 = req.body.slidingScale5,
+    carbRatio = req.body.carbRatio,
+  } = req.query;
+  const sql =
+    `SET @id=?;SET @user_id=?;SET @timesPD=?;SET @chkNutrition=?;
+    SET @chkWeight=?;SET @height=?;SET @chkMeds=?;SET @chkMedsB=?;
+    SET @chkMedsL=?;SET @chkMedsD=?;SET @chkMedsBed=?;SET @chkInsulin=?;
+    SET @typInsulin=?;SET @chkBP=?;SET @chkSlidingScale=?;
+    SET @slidingScale1=?;SET @slidingScale2a=?;SET @slidingScale2b=?;
+    SET @slidingScale3a=?;SET @slidingScale3b=?;SET @slidingScale4a=?;
+    SET @slidingScale4b=?;SET @slidingScale5=?;SET @carbRatio=?;
+     CALL updatePreference(@id,@user_id,@timesPD,@chkNutrition,
+      @chkWeight,@height,@chkMeds,@chkMedsB,@chkMedsL,@chkMedsD,
+      @chkMedsBed,@chkInsulin,@typInsulin,@chkBP,@chkSlidingScale,
+      @slidingScale1,@slidingScale2a,@slidingScale2b,@slidingScale3a,
+      @slidingScale3b,@slidingScale4a,@slidingScale4b,@slidingScale5,
+      @carbRatio);`;
 
   connection.query(
     sql,
@@ -65,8 +71,6 @@ router.post('/:id', (req, res) => {
       slidingScale4b,
       slidingScale5,
       carbRatio,
-      id,
-      user_id,
     ],
     (err, results) => {
       if (err) {
